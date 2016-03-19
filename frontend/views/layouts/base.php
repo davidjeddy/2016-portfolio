93fa6d36ed89bac5184d1c1e577f8a7018e439c0
David J Eddy <me@davidjeddy.com>
2016-03-19 11:16:46 -0400

    MODIFIED copyright to use getend() value. REMOVED  notice in footer.

--- frontend/views/layouts/base.php
+++ frontend/views/layouts/base.php
@@ -66,8 +66,8 @@ $this->beginContent('@frontend/views/layouts/_clear.php')
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?php echo date('Y') ?></p>
-        <p class="pull-right"><?php echo Yii::powered() ?></p>
+        <p class="pull-left">&copy; <?php echo getenv('COPYRIGHT_NAME'); ?> <?php echo date('Y') ?></p>
+        <p class="pull-right"><?php // echo Yii::powered() ?></p>
     </div>
 </footer>
 <?php $this->endContent() ?>
