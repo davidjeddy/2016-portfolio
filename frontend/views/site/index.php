415f3c0bdca5e487d798bcc77a1ff09768ba8f36
David E <me@davidjeddy.com>
2016-03-22 09:03:16 +0900

    ADDED yii2-portfolio-theme to composer.jso. ITERATED on FE/site/index VW

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,64 +1,102 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = Yii::$app->name;
+$content = \common\models\Page::find()
+    ->andWhere(['status' => 1])
+    ->orderBy(['title' => ASC])
+    ->all();
 ?>
-<div class="site-index">
 
-    <?php echo \common\widgets\DbCarousel::widget([
-        'key'=>'index',
-        'options' => [
-            'class' => 'slide', // enables slide effect
-        ],
+
+<?php /*echo \common\widgets\DbCarousel::widget([
+    'key'=>'index',
+    'options' => [
+        'class' => 'slide', // enables slide effect
+    ],
+]); */ ?>
+
+
+<!--
+<div class="jumbotron">
+    <h1>Congratulations!</h1>
+
+    <p class="lead">You have successfully created your Yii-powered application.</p>
+
+    <?php echo common\widgets\DbMenu::widget([
+        'key'=>'frontend-index',
+        'options'=>[
+            'tag'=>'p'
+        ]
     ]) ?>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+</div>
+-->
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <?php echo common\widgets\DbMenu::widget([
-            'key'=>'frontend-index',
-            'options'=>[
-                'tag'=>'p'
-            ]
-        ]) ?>
-
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">
+                <i class="fa fa-star-o"></i>  <?php echo getenv('COPYRIGHT_NAME'); ?>
+            </a>
         </div>
 
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+            <ul class="nav navbar-nav">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <?php foreach ($content as $key => $value) { ?>
+                <li>
+                    <a class="page-scroll" href="#<?php echo $value->slug; ?>"><?php echo $value->title; ?></a>
+                </li>
+                <?php }; ?>
+            </ul>
+        </div>
     </div>
-</div>
+</nav>
+
+<!-- Intro Header -->
+<header class="intro">
+    <div class="intro-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1 class="brand-heading"><?php echo getenv('COPYRIGHT_NAME'); ?></h1>
+                    <p class="intro-text">web developer, travel junky, craft beer lover, learning sponge.</p>
+                    <a href="#about" class="btn btn-circle page-scroll">
+                        <i class="fa fa-angle-double-down animated"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+<?php  foreach ($content as $key => $value) { ?>
+    <!-- About Section -->
+    <a name="<?php echo $value->slug; ?>"></a>
+    <section id="<?php echo $value->slug; ?>" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2><?php echo $value->title; ?></h2>
+                <p><?php echo $value->body; ?></p>
+            </div>
+        </div>
+    </section>
+<?php }; ?>
+
+
+<!-- Footer -->
+<section id="footer" class="container content-section text-center">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <p>Credits:</p>
+            <p>Photos provided by: <a href="http://gratisography.com/">Gratisography</a>, </p>
+        </div>
+    </div>
+</section>
